3005a0eef7eda9bd365e367532cf4e716c3d0769
Mike Thurgood <mikecthurgood@gmail.com>
2019-08-22 09:19:01 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -4,12 +4,14 @@ def nyc_pigeon_organizer(data)
     #split out original data into each attribute and it's details
     data.each do | attribute, attribute_details |
       
-      
-        attribute_details.each do | attributes, pigeon_names |
+    #further splits out each attribute detail into it's specific attributes and the name of the pigeon 
+        attribute_details.each do | specific_attributes, pigeon_names |
+          
+          #iterates through the pigeon names and acts on each one
             pigeon_names.each do | pigeon_name |
               pigeon_data_by_name[pigeon_name] ||= {}
               pigeon_data_by_name[pigeon_name][attribute] = []
-              pigeon_data_by_name[pigeon_name][attribute] << attributes.to_s
+              pigeon_data_by_name[pigeon_name][attribute] << specific_attributes.to_s
             end
         end
     end
